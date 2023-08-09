f07cf2ef0ce386d34fc3de307410902eb4f55825
onurkaymak <onurkaymak@outlook.com>
2023-08-08 22:39:21 -0700

    add converter function

--- scripts.js
+++ scripts.js
@@ -1,2 +1,9 @@
 const userInput = window.prompt('Say Something');
 window.alert(userInput);
+
+function converter(sentence) {
+    const firstLetter = sentence.slice(0, 1).toUpperCase();
+    const lastLetter = sentence.slice(-1).toUpperCase();
+    return firstLetter.concat(lastLetter);
+
+}
